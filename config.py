122f728238977acb7fe178f9b95a97a1df0f4878
shyamjalore <shyamparihar9587@gmail.com>
2024-05-30 02:41:34 +0530

    Add config

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'mysecret')
-    MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/mimage_database')
+    MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://doadmin:YN46J920joD173Cn@db-mongodb-blr1-35998-4e23f427.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=db-mongodb-blr1-35998')
     ADMIN_EMAIL = os.getenv('ADMIN_EMAIL', 'admin@example.com')
